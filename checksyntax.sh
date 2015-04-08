a3a0daad02a6cba0c80ffeaa59f1704822f74f30
Paul Ortchanian <Paul.Ortchanian@edelman.com>
2015-04-08 13:52:21 -0400

    added checksyntax and dploy.yaml, modified puppet init

---
+++ checksyntax.sh
@@ -0,0 +1,17 @@
+for f in  `git diff HEAD~1 --name-only |grep .php`
+
+do
+
+	if [ -a $f ] 
+	then
+		 result=`php.exe -l -f $f`
+		 if `echo $result| grep -q "No syntax errors detected"`; then
+			true #do nothing
+		 else
+			echo $result
+			exit -1
+		 fi 
+	fi 
+done
+
+exit 0
